73e4000891524a0954cca2b992a99f946ddcf0c2
Taeki <terenteva1999@yandex.ru>
2023-01-10 19:56:21 +0300

    laba6

--- src/Laba.java
+++ src/Laba.java
@@ -8,10 +8,9 @@ public class Laba {
     }
     public static int maximum(int [] arr){
         int max = arr[0];
-         for(int i = 0; i < arr.length ; i++){
+          for(int i = 0; i < arr.length ; i++)
             if(max < arr[i])
                 max = arr[i];
-        }
         return max;
     }
     public static void show(int[] arr){
